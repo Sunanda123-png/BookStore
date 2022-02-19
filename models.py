bee695d30fc823606fc2daaf3e9879ae1ef48586
Sunanda123-png <sunanda02@gmail.com>
2022-02-19 15:37:24 +0530

    Add:Crud operation for bookstore app

--- models.py
+++ models.py
@@ -24,3 +24,19 @@ class UserModel(db.Model):
         return f"{self.user_name}:{self.first_name}:{self.last_name}:{self.contact_number}:{self.password}:{self.email_address}"
 
 
+class BookModel(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    price = db.Column(db.Integer())
+    author = db.Column(db.String(80))
+
+    def __init__(self, name, price, author):
+        self.name = name
+        self.price = price
+        self.author = author
+
+    def json(self):
+        return {"name": self.name, "price": self.price, "author": self.author}
+
